419934c99989f96344e9c2ce75e49222a221fb28
Htet Nandar <htetnandar.282198@gmail.com>
2026-03-16 15:53:35 +0800

    Add files via upload

---
+++ src/test/java/sg/edu/nus/spring_data_jpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

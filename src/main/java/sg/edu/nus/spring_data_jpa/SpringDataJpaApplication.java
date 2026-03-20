53110839c193e37f20005273736f2f35381674bb
Shannon Loh <sihua615@hotmail.com>
2026-03-20 15:30:17 +0800

    Remove unused import in SpringDataJpaApplication
    Removed unused import for SpringBootApplication.

--- src/main/java/sg/edu/nus/spring_data_jpa/SpringDataJpaApplication.java
+++ src/main/java/sg/edu/nus/spring_data_jpa/SpringDataJpaApplication.java
@@ -1,7 +1,6 @@
 package sg.edu.nus.spring_data_jpa;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {

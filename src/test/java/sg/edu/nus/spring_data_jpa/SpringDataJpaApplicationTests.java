7fafbefd029cbc7328e37af98edf689ee155ff8b
Shannon Loh <sihua615@hotmail.com>
2026-03-20 15:32:59 +0800

    Update SpringDataJpaApplicationTests.java

--- src/test/java/sg/edu/nus/spring_data_jpa/SpringDataJpaApplicationTests.java
+++ src/test/java/sg/edu/nus/spring_data_jpa/SpringDataJpaApplicationTests.java
@@ -1,7 +1,6 @@
 package sg.edu.nus.spring_data_jpa;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
